59e180d3e5f5c0c96787f5b9529f58b3b5e5ed71
Samantha John <pair+samantha@gethopscotch.com>
2012-01-03 21:16:14 +0000

    Add signup form to detail pages.

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,6 +1,10 @@
 class SignupsController < ApplicationController
   def create
     @signup = Signup.create(params[:signup])
-    redirect_to root_path, notice: "Thanks for signing up for our mailing list. We'll be in touch soon!"
+    if @signup.source == "homepage"
+      redirect_to root_path, notice: "Thanks for signing up for our mailing list. We'll be in touch soon!"
+    else
+      redirect_to product_detail_path(name: @signup.source), notice: "Thanks for signing up for our mailing list. We'll be in touch soon!"
+    end
   end
 end
--- spec/controllers/signups_controller_spec.rb
+++ spec/controllers/signups_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 describe SignupsController do
-  it "should create a new signup" do
-    post :create, signup: {source: "homepage", email: "foo@bar.com"}
-    Signup.last.email.should == "foo@bar.com"
-    response.should redirect_to root_path
-    flash[:notice].should == "Thanks for signing up for our mailing list. We'll be in touch soon!"
+  context "when the source is the homepage" do
+    it "should create a new signup and redirect to homepage" do
+      post :create, signup: {source: "homepage", email: "foo@bar.com"}
+      Signup.last.email.should == "foo@bar.com"
+      response.should redirect_to root_path
+      flash[:notice].should == "Thanks for signing up for our mailing list. We'll be in touch soon!"
+    end
+  end
 
+  context "when the source is a game" do
+    it "should redirect to the game page" do
+      post :create, signup: {source: "astro_defenders", email: "foo@bar.com"}
+      Signup.last.email.should == "foo@bar.com"
+      response.should redirect_to product_detail_path(name: "astro_defenders")
+      flash[:notice].should == "Thanks for signing up for our mailing list. We'll be in touch soon!"
+    end
   end
 
 end
