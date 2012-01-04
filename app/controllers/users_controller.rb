a7615ee3d3efecf8ecbe9e5ababb10082e4ab7fc
Samantha John <pair+samantha@gethopscotch.com>
2012-01-04 14:44:07 +0000

    login redirects to kits index page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def login
     user = User.find_by_token(params[:token])
     session[:user_id] = user.id
-    head :ok
+    redirect_to kits_path
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,7 @@ describe UsersController do
       user  = User.create(name: "Samantha", token: "123")
       get :login, token: "123"
       controller.session[:user_id].should == user.id
+      response.should redirect_to kits_path
     end
   end
 
