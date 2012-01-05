4ab38b76312414420849c1b06dc6c8f167113df1
Evan Farrar <evanfarrar@gmail.com>
2012-01-05 16:46:16 -0500

    Add solution to steps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120104151715) do
+ActiveRecord::Schema.define(:version => 20120105214409) do
 
   create_table "kits", :force => true do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20120104151715) do
     t.datetime "updated_at"
     t.string   "title"
     t.text     "hint"
+    t.text     "solution"
   end
 
   create_table "users", :force => true do |t|
