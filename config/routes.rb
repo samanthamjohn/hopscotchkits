7f5625eb834c111ee417ee40c91e279a055321e5
Samantha John <pair+samantha@gethopscotch.com>
2012-01-10 23:23:22 +0000

    Change homepage to new puppy program page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Hopscotch::Application.routes.draw do
   resources :steps
   match "login/:token", to: "users#login"
 
-  resources :signups, only: [:create, :index]
   resources :kits do
     resources :programs do
       member do
@@ -12,8 +11,5 @@ Hopscotch::Application.routes.draw do
     end
   end
   resources :programs
-  match "about", to: "welcome#about", as: "about"
-  match "products", to: "welcome#products", as: "products"
-  match "/products/:name", to: "welcome#product_detail", as: "product_detail"
-  root :to => 'welcome#index'
+  root :to => 'programs#new', kit_id: "puppy"
 end
--- spec/models/signup_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-describe Signup do
-  it { should validate_presence_of :email}
-  it { should validate_presence_of :source}
-end
