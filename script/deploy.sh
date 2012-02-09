0d469fa5f8c2647fb11d565e2c0b1d10513b6c96
Samantha John <pair+samantha@gethopscotch.com>
2012-02-09 22:32:17 +0000

    Fix deploy script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "production" ]; then
-  git push heroku master && heroku run rake db:migrate --app gethopscotch && heroku restart --app gethopscotch && autotag create 'production'
+  git push heroku master && heroku run rake db:migrate --app hopscotchkits && heroku restart --app hopscotchkits && autotag create 'production'
 else
   git push staging master && heroku run rake db:migrate --app kits-staging && heroku restart --app kits-staging && autotag create 'staging'
 fi
