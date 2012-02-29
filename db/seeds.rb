21169b1ff20431d84eb7aec1bc9e023bc930a3e1
Samantha John <pair+samantha@gethopscotch.com>
2012-02-29 16:14:03 +0000

    Use ruby files to store data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-kits = JSON.parse(File.read(Rails.root.join("lib/kits.json")))
-kits.each do |kit_json|
-  kit = JSON.parse(kit_json)
-  if existing_kit = Kit.find_by_slug(kit["slug"])
-    existing_kit.update_attributes(kit)
-  else
-    Kit.create(kit)
-  end
-end
-
-steps = JSON.parse(File.read(Rails.root.join("lib/steps.json")))
-steps.each do |step_json|
-  step = JSON.parse(step_json)
-  if existing_step = Step.where(kit_id: step["kit_id"]).where(position: step["position"]).first
-    existing_step.update_attributes(step)
-  else
-    Step.create(step)
-  end
-end
-
+require Rails.root.join("lib/kits.rb")
+require Rails.root.join("lib/steps.rb")
 require Rails.root.join("lib/requirements.rb")
