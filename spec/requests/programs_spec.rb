4a46fee57e699802a74855100e117d2452d38d6b
Samantha John <pair+samantha@gethopscotch.com>
2012-02-29 19:06:11 +0000

    Fix integration specs.

--- spec/requests/programs_spec.rb
+++ spec/requests/programs_spec.rb
@@ -1,44 +1,40 @@
 require 'spec_helper'
 feature "hopscotch kits flow", js: true do
   background do
-    kit = Kit.create!(slug: "puppy")
-    step_1 = Step.create!(more_info: "a variable is what I say it is!", image_url: "step2.png", success_message: "You passed", title: "I love puppies", description: "hello from step 1", kit: kit, position: 1, spec: "assert(( -> true), 'Step 1 is passed')")
-    step_2 = Step.create!(success_message: "Yep", title: "I <3 kitties", description: "in between at step 2", kit: kit, position: 2, spec: "assert(( -> true), 'Step 2 is done')")
-    step_3 = Step.create!(success_message: "fuubar", title: "poop", description: "goodbye from step 3", kit: kit, position: 3, spec: "assert(( -> true), 'Step 3 is complete')")
+    require File.expand_path(Rails.root.join("db/seeds.rb"), __FILE__)
   end
-
   scenario "completing a kit" do
+    steps = Kit.find_by_slug("puppy").steps.order("position ASC").all
     visit root_path
     fill_in "What's your name?", with: "Samantha"
-    page.execute_script("window.editor.getSession().setValue('makeWorkspace()')")
-    click_on("Run this")
-    within_frame('stage') do
-      Nokogiri::HTML.parse(page.body).css('ellipse')
-    end
     click_on "Start coding"
 
     # step 1
     within ".preface > h1" do
       page.should have_content('Step 1: ')
       within "span.subtitle" do
-        page.should have_content("I love puppies")
+        page.should have_content(steps[0].title)
       end
     end
-    page.should have_css("img[src='/assets/step_images/step2.png']")
     click_on("more info")
     within "#more_info" do
-      page.should have_content("a variable is what I say it is!")
+      page.should have_content(steps[0].more_info.split('. ').last)
     end
     click_on("less info")
     page.should have_css("#more_info", visible: false)
 
-    page.execute_script("window.editor.getSession().setValue('makeWorkspace()')")
+    within ".specs" do
+      page.should have_css("div.unchecked")
+      page.should have_content(steps[0].requirements.first.name)
+    end
+
+    page.execute_script("window.editor.getSession().setValue('#{steps[0].solution}')")
     click_on("run this")
     within_frame('stage') do
       Nokogiri::HTML.parse(page.body).css('svg')
     end
     within ".progress .success" do
-      page.should have_content("You passed")
+      page.should have_content(steps[0].success_message)
     end
 
     # step 2
@@ -46,33 +42,33 @@ feature "hopscotch kits flow", js: true do
     within ".preface > h1" do
       page.should have_content('Step 2: ')
       within "span.subtitle" do
-        page.should have_content("I <3 kitties")
+        page.should have_content(steps[1].title)
       end
     end
+    page.should have_css("img[src='/assets/step_images/#{steps[1].image_url}']")
     page.should have_no_css("#more_info")
 
-    page.execute_script("window.editor.getSession().setValue('makeWorkspace().ellipse(0,0,10,10)')")
+    page.execute_script("window.editor.getSession().setValue('#{steps[1].solution.gsub("\r", ";").gsub("\n", ";")}')")
     click_on("run this")
-    within_frame('stage') do
-      Nokogiri::HTML.parse(page.body).css('ellipse')
-    end
     within ".progress .success" do
-      page.should have_content("Yep")
+      page.should have_content(steps[1].success_message.gsub(/<(\W|\w)(\w)+>/, '').strip)
     end
 
-    # step 3
-    click_on("Next")
-    click_on("run this")
-    within_frame('stage') do
-      Nokogiri::HTML.parse(page.body).css('svg')
+    steps[2, (steps.length - 3)].each do |step|
+      click_on "Next"
+      within ".preface > h1" do
+        page.should have_content("Step #{step.position}: ")
+        within "span.subtitle" do
+          page.should have_content(step.title)
+        end
+      end
+      page.execute_script("window.editor.getSession().setValue('#{step.solution.gsub("\r", ";").gsub("\n", ";")}')")
+      click_on("run this")
+      within ".progress .success" do
+        page.should have_content(step.success_message.gsub(/<(\W|\w)(\w)+>/, '').gsub(/<a.*/, '').gsub(/&deg;.*/, '').strip)
+      end
     end
-    within ".progress .success" do
-      page.should have_content("fuubar")
-      page.should have_css("a[href='/programs/#{Program.last.id}']")
-    end
-    within ".progress" do
-      page.should have_content("Complete!")
-    end
+
     click_on("Next")
     current_path.should == program_path(Program.last)
 
