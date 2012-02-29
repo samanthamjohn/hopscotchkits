45eacf141f664d7b4ac190c52208a316679c66e1
Samantha John <pair+samantha@gethopscotch.com>
2012-02-29 19:12:56 +0000

    Add seeds to the rakefile.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == "production" ]; then
-  git push heroku master && heroku run rake db:migrate --app hopscotchkits && heroku restart --app hopscotchkits && autotag create 'production'
+  git push heroku master && heroku run rake db:migrate --app hopscotchkits && heroku restart --app hopscotchkits && heroku run rake db:seed --app hopscotchkits && autotag create 'production'
 else
-  git push staging master && heroku run rake db:migrate --app kits-staging && heroku restart --app kits-staging && autotag create 'staging'
+  git push staging master && heroku run rake db:migrate --app kits-staging && heroku restart --app kits-staging && heroku run rake db:seed --app kits-staging && autotag create 'staging'
 fi
 
