8736ace34d9e538f8d3040acc6c9070bb8104ad2
Evan Farrar & Samantha John <pair+evan+samantha@gethopscotch.com>
2012-03-12 11:45:06 -0400

    Fix assets precompile.

--- config/application.rb
+++ config/application.rb
@@ -47,5 +47,7 @@ module Hopscotch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
