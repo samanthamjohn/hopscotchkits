43cd5dcc02bbb7ac63c2b916214f5ecfa0c5907e
Samantha John <pair+samantha@gethopscotch.com>
2012-03-20 15:36:58 +0000

    Order featured programs by updated at.

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -2,6 +2,6 @@ class KitsController < ApplicationController
 
   def index
     @kits = Kit.published
-    @programs = Program.featured.limit(6)
+    @programs = Program.featured.order('updated_at DESC').limit(6)
   end
 end
